1332e51efb13dcbe25e189158771f0b11b39315c
Virendra Gupta <virendrag@virendrag-macbookair1.roam.corp.google.com>
2016-03-17 18:57:35 +0530

    First commit

--- Testing/ViewController.m
+++ Testing/ViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+//    NSURL *baseURL = [NSURL URLWithString:@"https://partnersvtoolkit.appspot.com/get/"];
+    
+    NSURL *baseURL = [NSURL URLWithString:@"https://partnersvtoolkit.appspot.com/getExhibits"];
+    NSLog(@"***%@",baseURL);
+    
+    [self stringWithUrl:baseURL];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +33,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSString *)stringWithUrl:(NSURL *)url
+{
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url
+                                                cachePolicy:NSURLRequestReturnCacheDataElseLoad
+                                            timeoutInterval:30];
+    // Fetch the JSON response
+    NSData *urlData;
+    NSURLResponse *response;
+    NSError *error;
+    
+    // Make synchronous request
+    urlData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                    returningResponse:&response
+                                                error:&error];
+    
+   // NSLog(@"%@",urlData);
+    
+    
+    // Construct a String around the Data from the response
+    
+    NSLog(@"pp%@",[[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding]);
+    return [[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding];
+}
 @end
